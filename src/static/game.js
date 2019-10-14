bede323b0a6bed83a414d2ce994346d0aa08b471
Sibren <0924655@hr.nl>
2019-10-14 16:28:35 +0200

    add bird

--- src/static/game.js
+++ src/static/game.js
@@ -36,11 +36,12 @@ document.addEventListener('keyup', function (event) {
             break;
     }
 });
+
 var socket = io();
 socket.on('message', function (data) {
     console.log(data);
 });
-if(!document.URL.endsWith('overview')){
+if (!document.URL.endsWith('overview')) {
     socket.emit('new player');
 }
 
@@ -57,8 +58,13 @@ socket.on('state', function (players) {
     context.fillStyle = 'green';
     for (var id in players) {
         var player = players[id];
-        context.beginPath();
-        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-        context.fill();
+        var img = document.createElement('img');
+
+        img.onload = function () {
+            ctx.drawImage(this, 0, 0);
+        }
+
+        img.src = 'pictures/Bird.svg';
+        img.width = 60; img.height = 60;
     }
 });
