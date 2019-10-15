68b456d3f4acab97822280e3221baaee334e4186
Nencio Paniagua <nencio.paniagua@casema.nl>
2019-10-15 11:36:31 +0200

    Onnodige zooi weggehaald

--- src/static/game.js
+++ src/static/game.js
@@ -45,40 +45,6 @@ function buttonRightEnd()
     movement.right = false
 }
 
-/* document.addEventListener('keydown', function (event) {
-    switch (event.keyCode) {
-        case 65: // A
-            movement.left = true;
-            break;
-        case 87: // W
-            movement.up = true;
-            break;
-        case 68: // D
-            movement.right = true;
-            break;
-        case 83: // S
-            movement.down = true;
-            break;
-    }
-}); */
-
-/* document.addEventListener('keyup', function (event) {
-    switch (event.keyCode) {
-        case 65: // A
-            movement.left = false;
-            break;
-        case 87: // W
-            movement.up = false;
-            break;
-        case 68: // D
-            movement.right = false;
-            break;
-        case 83: // S
-            movement.down = false;
-            break;
-    }
-}); */
-
 var socket = io();
 socket.on('message', function (data) {
     console.log(data);
